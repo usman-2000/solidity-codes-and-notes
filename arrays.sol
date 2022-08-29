625695f6300ddddcef1a9033e1a402dd2768775a
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-08-30 00:32:05 +0500

    Update arrays.sol

--- arrays.sol
+++ arrays.sol
@@ -18,3 +18,22 @@ contract arrays{
         return nums.length;
     }
 }
+
+
+
+
+
+contract bytesdetails{
+    // bytes k ander ham koi bhi value deingay to wo uski hexadecimal value return kryga
+    // bytes k andr usky size ki values hi de skty hain jese k bytes1 mn srf aik value de skty
+
+    bytes1 public b1;
+    bytes2 public b2;
+    bytes3 public b3;
+
+    function setBytes() public {
+        b1 = 'a';
+        b2 = 'ab';
+        b3 = 'abc';
+    }
+}
