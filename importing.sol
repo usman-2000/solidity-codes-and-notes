fec7533d69193bed979bd3db87a52c882fd94838
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-09-14 16:26:38 +0500

    Create importing.sol

---
+++ importing.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Mapping.sol";
+
+contract Importing_a_Contract{
+    MAPPINGS public  mappings;
+
+    function createMappingsContract() public {
+        mappings = new MAPPINGS();
+    }
+
+
+}
