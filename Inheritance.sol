5328961d3a5c7cdbaa60b8ea722ede5ab95c3d41
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-09-16 23:06:49 +0500

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -5,4 +5,17 @@ import "./SimpleStorage.sol";
 
 contract ExtraStorage is SimpleStorageContract{
 
+    // override:    if a parent contract has the same function , so in solidity we should 
+    //              use override while making changings in that function but in child contract
+
+    // virtual:     when we are overriding a function to jo parent contract hoga us mn 
+    //              us specific function py virtual ka keyword lazmi lagaya
+    //              hona chahiye ,yeh us function ko overrideable banata hai
+
+
+    // so in this function we are overriding by adding 5 in a favorite Number
+
+    function store(uint256 _favoriteNumber) public override{
+        FavoriteNumber = _favoriteNumber + 5;
+    }
 }
